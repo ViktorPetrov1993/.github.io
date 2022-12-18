3d54425b579c5760f0c68a05a0b219e513abfc72
viktor <viktorpetrov1993s@gmail.com>
2022-12-18 15:38:17 +0200

    deploy

---
+++ js/script.js
@@ -0,0 +1,71 @@
+// let denis2 = ['denis', 2, 'j', 7]
+// let denis = {
+//     // let = obyavlenie peremennoi ona hranit obekt (vse chto v figyrnih skobkah)asociativniy masiv
+//     name: 'denis', 
+//     surname: 1,
+//     denis3: denis2
+// }
+// console.log(denis2[0])
+// // stroka chislo massiv obekt - tipi dannih
+
+// for (let i = 0; i < denis2.length; i++) {
+//     console.log('cicle', denis2[i]) 
+// }
+
+// function test(x, y) {
+//     if (!x && !y) {
+//         alert('idi nahui')
+//     } else {
+//         console.log(x+y)
+//     }
+// }
+
+// test()
+
+// window.onload = function () {
+//     const menu = document.getElementsByClassName('leftblock_content')
+//     console.log(menu[0].children[1])
+// }
+// let denis ='stroka'
+// let denis1 = 1
+// let denis3 = {
+//     prop1: 'string'
+// }
+// let denis4 = [1, 2, 3]
+// let true1 = true
+// let test = null
+// let test1 = undefined
+
+// function tesst (x, y) {
+//     console.log(x + y)
+
+//     return x + y
+// }
+// const result = tesst(3, 5)
+// console.log('CONSTQNT: ', result)
+
+
+// window.onload = function () {
+//     particlesJS.load('particles-js', 'js/options.json', function () {
+//         console.log('callback - particles.js config loaded');
+//     });
+//  }  
+
+//  const stc = require('string-to-color');
+// const color = stc('denis');
+
+// let people = [
+//     {name: 'Vytia'},
+//     {name: 'Vytia222'},
+//     {name: 'Vytia333'},
+//     {name: 'Vytia333'},
+// ]
+
+// for (let i=0; i<people.length; i++) {
+//     const new_color = stc(people[i].name)
+//     console.log(new_color)
+// }
+
+window.onload = () => {  document.querySelector('[src="img/Right-Image.jpg"]').onclick = () => document.querySelector('[src="img/Right-Image.jpg"]').src = 'img/Image-1.jpg' }
+
+
